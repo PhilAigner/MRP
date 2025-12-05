9f5ed0f73c4120666ebe2f7ce9c6e499a24fb8a2
Philipp Aigner <if25b196@technikum-wien.at>
2025-12-05 15:55:50 +0100

    removed usings

--- Handler.cs
+++ Handler.cs
@@ -1,8 +1,4 @@
-﻿using System.Reflection.PortableExecutable;
-using System.Runtime.CompilerServices;
-using System.Runtime.ConstrainedExecution;
-using System.Security.Cryptography.X509Certificates;
-using DotNetEnv;
+﻿using DotNetEnv;
 
 namespace MRP
 {
@@ -42,61 +38,6 @@ namespace MRP
             mediaRepository = new MediaRepository(dbConnection, userRepository);
             ratingRepository = new RatingRepository(dbConnection);
             tokenService = new TokenService();
-
-            //SAMPLE CODE TO SHOWCASE Repositories
-
-            /*
-
-            Console.WriteLine("\n---Users---");
-
-            /////////////////////////////
-            //create sample user entries
-            userRepository.AddUser(new User("USer1", "pwd", profileRepository));
-            userRepository.AddUser(new User("User2", "pwd2", profileRepository));
-            userRepository.AddUser(new User("User3", "pwd3", profileRepository));
-
-            //print all users
-            List<User> users = userRepository.GetAll();
-            foreach (var thing in users)
-            {
-                Console.WriteLine($"Username: {thing.username}, UUID: {thing.uuid}");
-            }
-
-
-            Console.WriteLine("\n---Media---");
-
-            /////////////////////////////
-            //create sample media entries
-            Guid media1 = mediaRepository.AddMedia(new MediaEntry("Title1", EMediaType.Movie, 2001, EFSK.FSK0, "Action", users[0]));
-            Guid media2 = mediaRepository.AddMedia(new MediaEntry("Title2", EMediaType.Movie, 2002, EFSK.FSK18, "Action", users[1]));
-            Guid media3 = mediaRepository.AddMedia(new MediaEntry("Title3", EMediaType.Movie, 2003, EFSK.FSK18, "Action", users[2]));
-
-            //print all media entries
-            List<MediaEntry> media = mediaRepository.GetAll();
-            foreach (var thing in media)
-            {
-                Console.WriteLine($"Title: {thing.title}, UUID: {thing.uuid}");
-            }
-
-
-
-            Console.WriteLine("\n---Ratings---");
-
-            ////////////////////////////
-            //create sample rating data
-            ratingRepository.AddRating(new Rating(media[0].uuid, users[0].uuid, 1));
-            ratingRepository.AddRating(new Rating(media[1].uuid, users[1].uuid, 2));
-            ratingRepository.AddRating(new Rating(media[2].uuid, users[2].uuid, 3));
-
-            //print all rating entries
-            List<Rating> ratings = ratingRepository.GetAll();
-            foreach (var thing in ratings)
-            {
-                Console.WriteLine($"Rating from: {thing.user} on {thing.mediaEntry} - {thing.stars} stars, with UUID: {thing.uuid}");
-            }
-
-            */
-
         }
 
         public async Task<int> StartAsync()
@@ -115,7 +56,7 @@ namespace MRP
                 new RatingsHTTPEndpoint(ratingRepository, userRepository, mediaRepository, profileRepository, tokenService),
             };
 
-            // Ändern Sie localhost zu 0.0.0.0 für Container-Kompatibilität
+            // Server runs on localhost for local development
             await HttpServer.RunServer("http://127.0.0.1:8080/api/", HttpEndpoints);
 
             return 0;
